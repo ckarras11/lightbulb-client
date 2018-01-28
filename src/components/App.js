9e70cf5bd642e1bcbb82f2e373f8e2308a1c1517
Christopher Karras <ckarras11@gmail.com>
2018-01-27 20:23:37 -0500

    Added react router and multiple 'pages'

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Landing from './landing';
+import Login from './login';
+import Register from './register';
+import Profile from './profile';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      HOME
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Landing} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/home' component={Profile} />
+        </div>
+      </Router>
     );
   }
 }
