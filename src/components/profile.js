0e28874975fb115ea3a2105f942a840073c8efad
Christopher Karras <ckarras11@gmail.com>
2018-02-05 13:01:56 -0500

    Connected profile with state for user info

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Profile = () => {
-    return (
-        <div>
-            <h2>Profile</h2>
-        </div>
-    );
+function mapStateToProps(state) {
+    return {
+        userName: state.reducer.userName,
+    };
+}
+
+export class Profile extends React.Component{
+    render() {
+        return (
+            <div>
+                <h2>Profile</h2>
+                <h3>{this.props.userName}</h3>
+            </div>
+        );
+    } 
 };
 
-export default Profile;
+export default connect(mapStateToProps)(Profile);
