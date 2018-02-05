b5ad97e8ab7b02daff29716fb6c9d68047839cbf
Christopher Karras <ckarras11@gmail.com>
2018-02-05 13:01:27 -0500

    Connected browse with reducer to populate ideas

--- src/components/browse.js
+++ src/components/browse.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import StickyNote from './sticky-note';
 
-const Browse = () => {
-    return (
-        <div>
-            <h2>Browse</h2>
-            <ul className='sticky-board'>
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
-                <StickyNote title='test' content='Id ullamco tempor Lorem eiusmod reprehenderit dolor enim officia. Excepteur nulla esse elit dolor Lorem dolore ipsum ipsum. Culpa sunt aute tempor tempor. Occaecat consectetur deserunt sit est do fugiat enim. Ipsum qui ut sit anim nisi proident ex incididunt culpa et ex excepteur et reprehenderit.' />
+function mapStateToProps(state) {
+    return {
+        ideas: state.reducer.ideas,
+    };
+}
 
-            </ul>
-        </div>
-    );
+export class Browse extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        const ideas = this.props.ideas.map((idea, index) => {
+            return <StickyNote key={index} title={idea.title} content={idea.content} />
+        })
+        return (
+            <div>
+                <h2>Browse</h2>
+                <ul className='sticky-board'>
+                    {ideas}
+                </ul>
+            </div>
+        );
+    }  
 };
 
-export default Browse;
+export default connect(mapStateToProps)(Browse);
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,18 @@
 //import actions
 
-const initialState = {}
+const initialState = {
+    ideas: [
+        {title: 'Test #1', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+        {title: 'Test #2', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+        {title: 'Test #3', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+        {title: 'Test #4', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+        {title: 'Test #5', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+        {title: 'Test #6', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+        {title: 'Test #7', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do'},
+
+    ],
+    userName: 'John Doe'
+}
 
 const reducer = (state = initialState, action) => {
     return state;
